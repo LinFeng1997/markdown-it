3bbd90d52c52ff2fa8de0b9467bc430c8ab675b5
许云峰 <xuyunfeng.fe@bytedance.com>
2019-05-11 16:42:55 +0800

    Refactory:balance_pairs rule to ts

--- lib/rules_inline/balance_pairs.ts
+++ lib/rules_inline/balance_pairs.ts
@@ -1,19 +1,21 @@
 // For each opening emphasis-like marker find a matching closing one
 //
 'use strict';
+import StateInline from "./state_inline";
+import MarkdownIt = require("../../index");
 
+module.exports = function link_pairs(state: StateInline): void {
+    let lastDelim: MarkdownIt.Delimiter,
+        currDelim: MarkdownIt.Delimiter,
+        delimiters = state.delimiters,
+        max: number = state.delimiters.length;
 
-module.exports = function link_pairs(state) {
-  var i, j, lastDelim, currDelim,
-      delimiters = state.delimiters,
-      max = state.delimiters.length;
-
-  for (i = 0; i < max; i++) {
+  for (let i = 0; i < max; i++) {
     lastDelim = delimiters[i];
 
     if (!lastDelim.close) { continue; }
 
-    j = i - lastDelim.jump - 1;
+    let j = i - lastDelim.jump - 1;
 
     while (j >= 0) {
       currDelim = delimiters[j];
