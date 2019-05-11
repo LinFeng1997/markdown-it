5268a004ee18273ba2f799f424c035fa25a5c7b9
许云峰 <xuyunfeng.fe@bytedance.com>
2019-05-11 14:00:27 +0800

    chore:Token type

--- types/token.d.ts
+++ types/token.d.ts
@@ -1,21 +1,2 @@
-export default class Token {
-    constructor(type: string, tag: string, nesting: number);
-    attrGet: (name: string) => string | null;
-    attrIndex: (name: string) => number;
-    attrJoin: (name: string, value: string) => void;
-    attrPush: (attrData: string[]) => void;
-    attrSet: (name: string, value: string) => void;
-    attrs: string[][] | null;
-    block: boolean;
-    children: Token[] | null;
-    content: string;
-    hidden: boolean;
-    info: string;
-    level: number;
-    map: number[] | null;
-    markup: string;
-    meta: any;
-    nesting: number;
-    tag: string;
-    type: string;
-}
+import Token = require('../lib/token');
+export default Token;
