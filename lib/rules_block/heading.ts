08c328b9ba5558682c0503d4a24abd2af35c41cb
许云峰 <xuyunfeng.fe@bytedance.com>
2019-06-27 18:54:16 +0800

    Refactory:heading rule

--- lib/rules_block/heading.ts
+++ lib/rules_block/heading.ts
@@ -8,39 +8,44 @@ import Token = require('../token');;
 const isSpace = require('../common/utils').isSpace;
 
 module.exports = function heading(state: StateBlock, startLine: number, endLine: number, silent: boolean) {
-  let ch: number,
-      level: number,
-      tmp: number,
-      token: Token,
+  let token: Token,
       pos: number = state.bMarks[startLine] + state.tShift[startLine],
       max: number = state.eMarks[startLine];
 
   // if it's indented more than 3 spaces, it should be a code block
-  if (state.sCount[startLine] - state.blkIndent >= 4) { return false; }
+  if (state.isMoreIndent(startLine)) { return false; }
 
-  ch  = state.src.charCodeAt(pos);
+  let ch  = state.src.charCodeAt(pos);
 
   if (ch !== 0x23/* # */ || pos >= max) { return false; }
 
-  // count heading level
-  level = 1;
-  ch = state.src.charCodeAt(++pos);
-  while (ch === 0x23/* # */ && pos < max && level <= 6) {
-    level++;
+  function countLevel(){
+    // count heading level
+    let level = 1;
     ch = state.src.charCodeAt(++pos);
+    while (ch === 0x23/* # */ && pos < max && level <= 6) {
+      level++;
+      ch = state.src.charCodeAt(++pos);
+    }
+    return level;
   }
+
+  function cutTails(){
+    max = state.skipSpacesBack(max, pos);
+    let tmp = state.skipCharsBack(max, 0x23, pos); // #
+    if (tmp > pos && isSpace(state.src.charCodeAt(tmp - 1))) {
+      max = tmp;
+    }
+  }
+  // count heading level
+  let level = countLevel();
 
   if (level > 6 || (pos < max && !isSpace(ch))) { return false; }
 
   if (silent) { return true; }
 
   // Let's cut tails like '    ###  ' from the end of string
-
-  max = state.skipSpacesBack(max, pos);
-  tmp = state.skipCharsBack(max, 0x23, pos); // #
-  if (tmp > pos && isSpace(state.src.charCodeAt(tmp - 1))) {
-    max = tmp;
-  }
+  cutTails();
 
   state.line = startLine + 1;
 
