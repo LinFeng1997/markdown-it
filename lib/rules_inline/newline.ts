fb6b4b1d66038621427e1aedf36abb5714838ee1
许云峰 <xuyunfeng.fe@bytedance.com>
2019-06-30 21:08:08 +0800

    Refactory:newline rule

--- lib/rules_inline/newline.ts
+++ lib/rules_inline/newline.ts
@@ -6,32 +6,38 @@ import StateInline from "./state_inline";
 var isSpace = require('../common/utils').isSpace;
 
 module.exports = function newline(state: StateInline, silent: boolean): boolean {
-  let pmax: number,
-    max: number,
-    pos: number = state.pos;
+  let pos: number = state.pos;
 
   if (state.src.charCodeAt(pos) !== 0x0A/* \n */) { return false; }
 
-  pmax = state.pending.length - 1;
-  max = state.posMax;
+  function isWhite(pmax){
+    return pmax >= 0 && state.pending.charCodeAt(pmax) === 0x20;
+  }
+
+  function getBreak(){
+    if (isWhite(state.pending.length - 1)) {
+      if (isWhite(state.pending.length - 2)) {
+        state.pending = state.pending.replace(/ +$/, '');
+        return {type: 'hardbreak', tag: 'br', nesting: 0}
+      } else {
+        state.pending = state.pending.slice(0, -1);
+        return {type: 'softbreak', tag: 'br', nesting: 0}
+      }
+
+    } else {
+      return {type: 'softbreak', tag: 'br', nesting: 0}
+    }
+  }
+
+  let max = state.posMax;
 
   // '  \n' -> hardbreak
   // Lookup in pending chars is bad practice! Don't copy to other rules!
   // Pending string is stored in concat mode, indexed lookups will cause
   // convertion to flat mode.
   if (!silent) {
-    if (pmax >= 0 && state.pending.charCodeAt(pmax) === 0x20) {
-      if (pmax >= 1 && state.pending.charCodeAt(pmax - 1) === 0x20) {
-        state.pending = state.pending.replace(/ +$/, '');
-        state.push('hardbreak', 'br', 0);
-      } else {
-        state.pending = state.pending.slice(0, -1);
-        state.push('softbreak', 'br', 0);
-      }
-
-    } else {
-      state.push('softbreak', 'br', 0);
-    }
+    const {type, tag, nesting} = getBreak();
+    state.push(type, tag, nesting);
   }
 
   pos++;
