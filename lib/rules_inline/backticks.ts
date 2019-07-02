deae817c1291164826bec5399f77778873992732
许云峰 <xuyunfeng.fe@bytedance.com>
2019-07-02 18:56:33 +0800

    Refactory:backtick rule

--- lib/rules_inline/backticks.ts
+++ lib/rules_inline/backticks.ts
@@ -5,26 +5,29 @@ import StateInline from "./state_inline";
 import Token = require("../token");
 
 module.exports = function backtick(state: StateInline, silent: boolean): boolean {
-  let start: number,
-    max: number,
-    marker: string,
-    matchStart: number,
-    matchEnd: number,
+  let matchStart: number,
     token: Token,
     pos: number = state.pos,
     ch: number = state.src.charCodeAt(pos);
 
   if (ch !== 0x60/* ` */) { return false; }
 
-  start = pos;
-  pos++;
-  max = state.posMax;
+  function genToken(){
+    token         = state.push('code_inline', 'code', 0);
+    token.markup  = marker;
+    token.content = state.src.slice(pos, matchStart)
+        .replace(/[ \n]+/g, ' ')
+        .trim();
+  }
+
+  let start = pos++;
+  let max = state.posMax;
 
   while (pos < max && state.src.charCodeAt(pos) === 0x60/* ` */) { pos++; }
 
-  marker = state.src.slice(start, pos);
+  let marker = state.src.slice(start, pos);
 
-  matchStart = matchEnd = pos;
+  let matchEnd = pos;
 
   while ((matchStart = state.src.indexOf('`', matchEnd)) !== -1) {
     matchEnd = matchStart + 1;
@@ -33,11 +36,7 @@ module.exports = function backtick(state: StateInline, silent: boolean): boolean
 
     if (matchEnd - matchStart === marker.length) {
       if (!silent) {
-        token         = state.push('code_inline', 'code', 0);
-        token.markup  = marker;
-        token.content = state.src.slice(pos, matchStart)
-                                 .replace(/[ \n]+/g, ' ')
-                                 .trim();
+        genToken();
       }
       state.pos = matchEnd;
       return true;
