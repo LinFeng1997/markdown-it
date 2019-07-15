71cde86cf98af9a80fecdeaa07a0148f5d688420
许云峰 <xuyunfeng.fe@bytedance.com>
2019-07-15 20:55:15 +0800

    Refactory:html_inline rule

--- lib/rules_inline/html_inline.ts
+++ lib/rules_inline/html_inline.ts
@@ -3,7 +3,6 @@
 
 
 import StateInline from "./state_inline";
-import Token = require("../token");
 
 var HTML_TAG_RE = require('../common/html_re').HTML_TAG_RE;
 
@@ -14,38 +13,38 @@ function isLetter(ch) {
   return (lc >= 0x61/* a */) && (lc <= 0x7a/* z */);
 }
 
+function isFailedSecondCh(ch) {
+  return ch !== 0x21/* ! */ &&
+      ch !== 0x3F/* ? */ &&
+      ch !== 0x2F/* / */ &&
+      !isLetter(ch)
+}
 
 module.exports = function html_inline(state: StateInline, silent: boolean): boolean {
-  let ch: number,
-    match: RegExpMatchArray | null,
-    max: number,
-    token: Token,
-
-      pos = state.pos;
+  let pos = state.pos;
 
   if (!state.md.options.html) { return false; }
 
+  function isStart(){
+    return state.src.charCodeAt(pos) === 0x3C/* < */ &&
+        pos + 2 < state.posMax
+  }
+
   // Check start
-  max = state.posMax;
-  if (state.src.charCodeAt(pos) !== 0x3C/* < */ ||
-      pos + 2 >= max) {
+  if (!isStart()) {
     return false;
   }
 
   // Quick fail on second char
-  ch = state.src.charCodeAt(pos + 1);
-  if (ch !== 0x21/* ! */ &&
-      ch !== 0x3F/* ? */ &&
-      ch !== 0x2F/* / */ &&
-      !isLetter(ch)) {
+  if (isFailedSecondCh(state.src.charCodeAt(pos + 1))) {
     return false;
   }
 
-  match = state.src.slice(pos).match(HTML_TAG_RE);
+  let match = state.src.slice(pos).match(HTML_TAG_RE);
   if (!match) { return false; }
 
   if (!silent) {
-    token         = state.push('html_inline', '', 0);
+    let token         = state.push('html_inline', '', 0);
     token.content = state.src.slice(pos, pos + match[0].length);
   }
   state.pos += match[0].length;
