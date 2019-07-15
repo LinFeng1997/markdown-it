71b22e7953cffd0df99d030d31c764d8a24ff2b9
许云峰 <xuyunfeng.fe@bytedance.com>
2019-07-15 20:40:31 +0800

    Refactory:autolink rule

--- lib/rules_inline/autolink.ts
+++ lib/rules_inline/autolink.ts
@@ -2,7 +2,6 @@
 
 
 import StateInline from "./state_inline";
-import Token = require("../token");
 
 /*eslint max-len:0*/
 const EMAIL_RE    = /^<([a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*)>/;
@@ -10,69 +9,46 @@ const AUTOLINK_RE = /^<([a-zA-Z][a-zA-Z0-9+.\-]{1,31}):([^<>\x00-\x20]*)>/;
 
 
 module.exports = function autolink(state: StateInline, silent: boolean): boolean {
-  let tail: string,
-    linkMatch: RegExpMatchArray,
-    emailMatch: RegExpMatchArray,
-    url: string = '',
-    fullUrl: string,
-    token: Token,
-    pos = state.pos;
+  let pos = state.pos;
 
   if (state.src.charCodeAt(pos) !== 0x3C/* < */) { return false; }
 
-  tail = state.src.slice(pos);
+  function getToken(href,link){
+    let token         = state.push('link_open', 'a', 1);
+    token.attrs   = [ [ 'href', href ] ];
+    token.markup  = 'autolink';
+    token.info    = 'auto';
+
+    token         = state.push('text', '', 0);
+    token.content = state.md.normalizeLinkText(link);
+
+    token         = state.push('link_close', 'a', -1);
+    token.markup  = 'autolink';
+    token.info    = 'auto';
+    return token;
+  }
+
+  function isAutoLink(reg,prefix=''){
+    if (reg.test(tail)) {
+      let linkMatch = tail.match(reg) || [];
+
+      let url = linkMatch[0].slice(1, -1);
+      let fullUrl = state.md.normalizeLink(prefix + url);
+      if (!state.md.validateLink(fullUrl)) { return false; }
+
+      if (!silent) {
+        getToken(fullUrl,url);
+      }
+
+      state.pos += linkMatch[0].length;
+      return true;
+    }
+    return false;
+  }
+
+  let tail = state.src.slice(pos);
 
   if (tail.indexOf('>') < 0) { return false; }
 
-  if (AUTOLINK_RE.test(tail)) {
-    linkMatch = tail.match(AUTOLINK_RE) || [];
-
-    url = linkMatch[0].slice(1, -1);
-    fullUrl = state.md.normalizeLink(url);
-    if (!state.md.validateLink(fullUrl)) { return false; }
-
-    if (!silent) {
-      token         = state.push('link_open', 'a', 1);
-      token.attrs   = [ [ 'href', fullUrl ] ];
-      token.markup  = 'autolink';
-      token.info    = 'auto';
-
-      token         = state.push('text', '', 0);
-      token.content = state.md.normalizeLinkText(url);
-
-      token         = state.push('link_close', 'a', -1);
-      token.markup  = 'autolink';
-      token.info    = 'auto';
-    }
-
-    if(linkMatch) state.pos += linkMatch[0].length;
-    return true;
-  }
-
-  if (EMAIL_RE.test(tail)) {
-    emailMatch = tail.match(EMAIL_RE) || [];
-
-    url = emailMatch[0].slice(1, -1);
-    fullUrl = state.md.normalizeLink('mailto:' + url);
-    if (!state.md.validateLink(fullUrl)) { return false; }
-
-    if (!silent) {
-      token         = state.push('link_open', 'a', 1);
-      token.attrs   = [ [ 'href', fullUrl ] ];
-      token.markup  = 'autolink';
-      token.info    = 'auto';
-
-      token         = state.push('text', '', 0);
-      token.content = state.md.normalizeLinkText(url);
-
-      token         = state.push('link_close', 'a', -1);
-      token.markup  = 'autolink';
-      token.info    = 'auto';
-    }
-
-    state.pos += emailMatch[0].length;
-    return true;
-  }
-
-  return false;
+  return isAutoLink(AUTOLINK_RE) || isAutoLink(EMAIL_RE,'mailto:');
 };
