b9c02c6355ba3ec8beb1f46bf01e5a4d5adab2c7
许云峰 <xuyunfeng.fe@bytedance.com>
2019-07-16 20:30:29 +0800

    Refactory:balance_pairs rule

--- lib/rules_inline/balance_pairs.ts
+++ lib/rules_inline/balance_pairs.ts
@@ -2,42 +2,42 @@
 //
 
 import StateInline from "./state_inline";
-import MarkdownIt = require("../../types/index");
 
 module.exports = function link_pairs(state: StateInline): void {
-    let lastDelim: MarkdownIt.Delimiter,
-        currDelim: MarkdownIt.Delimiter,
-        delimiters = state.delimiters,
-        max: number = state.delimiters.length;
+  let delimiters = state.delimiters,
+      max: number = state.delimiters.length;
+
+  function canOpen(currDelim,lastDelim) {
+   return currDelim.open &&
+    currDelim.marker === lastDelim.marker &&
+    currDelim.end < 0 &&
+    currDelim.level === lastDelim.level
+  }
+
+  // typeofs are for backward compatibility with plugins
+  function oddMatch(currDelim,lastDelim) {
+    return (currDelim.close || lastDelim.open) &&
+        typeof currDelim.length !== 'undefined' &&
+        typeof lastDelim.length !== 'undefined' &&
+        (currDelim.length + lastDelim.length) % 3 === 0
+  }
 
   for (let i = 0; i < max; i++) {
-    lastDelim = delimiters[i];
+    let lastDelim = delimiters[i];
 
     if (!lastDelim.close) { continue; }
 
     let j = i - lastDelim.jump - 1;
 
     while (j >= 0) {
-      currDelim = delimiters[j];
+      let currDelim = delimiters[j];
 
-      if (currDelim.open &&
-          currDelim.marker === lastDelim.marker &&
-          currDelim.end < 0 &&
-          currDelim.level === lastDelim.level) {
-
-        // typeofs are for backward compatibility with plugins
-        var odd_match = (currDelim.close || lastDelim.open) &&
-                        typeof currDelim.length !== 'undefined' &&
-                        typeof lastDelim.length !== 'undefined' &&
-                        (currDelim.length + lastDelim.length) % 3 === 0;
-
-        if (!odd_match) {
+      if (canOpen(currDelim,lastDelim) && !oddMatch(currDelim,lastDelim)) {
           lastDelim.jump = i - j;
           lastDelim.open = false;
           currDelim.end  = i;
           currDelim.jump = 0;
           break;
-        }
       }
 
       j -= currDelim.jump + 1;
